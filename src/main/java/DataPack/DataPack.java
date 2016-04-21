f404c7cf70e0125a3f3a0469926161d23a81e539
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-04-21 21:28:26 +0800

    Add more commands, complete the protocol.

--- src/main/java/DataPack/DataPack.java
+++ src/main/java/DataPack/DataPack.java
@@ -12,7 +12,16 @@ public class DataPack {
     public final static int LOGIN = 1000;
     public final static int LOGOUT = 1002;
     public final static int REGISTER = 1003;
-    public final static int TERMINATE = 2000;
+    public final static int ROOM_ENTER = 2004;
+    public final static int ROOM_CREATE = 2005;
+    public final static int GAME_START = 2006;
+    public final static int GAME_PROCEED = 2007;
+    public final static int GAME_FINISHED = 2008;
+    public final static int ROOM_EXIT = 2009;
+    public final static int ROOM_LOOKUP = 3000;
+    public final static int ROOM_USER_ENTERED = 3001;
+    public final static int ROOM_USER_LEFT = 3002;
+    public final static int TERMINATE = 5000;
 
     private int command = 0;
     private Date date = null;
