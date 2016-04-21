72d734942611dd7a495fac580fa67defc5246796
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-04-21 21:29:20 +0800

    Add logic for all commands.

--- src/main/java/Server/UserCommunicationJob.java
+++ src/main/java/Server/UserCommunicationJob.java
@@ -2,6 +2,7 @@ package Server;
 
 import DataPack.DataPack;
 import Database.Database;
+import Room.Room;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.logging.log4j.LogManager;
@@ -78,6 +79,14 @@ class UserCommunicationJob implements Runnable {
                             List<String> msgList = new ArrayList<>();
                             msgList.add(userInfo.get(0));
                             msgList.add(userInfo.get(3));
+                            for(Room room : parent.getRooms()){
+                                // add room id
+                                msgList.add(String.valueOf(room.getId()));
+                                // add room name
+                                msgList.add(room.getName());
+                                // add number of players in the room
+                                msgList.add(String.valueOf(room.getUsers().size()));
+                            }
                             send(new DataPack(DataPack.LOGIN, new Date(), true, msgList));
                             parent.addUserCommunicationJob(Integer.valueOf(userInfo.get(0)), this);
                         }
@@ -102,14 +111,126 @@ class UserCommunicationJob implements Runnable {
                 }
                 case DataPack.LOGOUT:{
                     int userIndex = Integer.valueOf(dataPack.getMessage(0));
-
+                    parent.removeUserCommunicationJob(userIndex);
                     send(new DataPack(DataPack.LOGOUT, new Date(), true, null));
+                    return;
+                }
+                case DataPack.ROOM_CREATE:{
+                    Integer userId = Integer.valueOf(dataPack.getMessage(0));
+                    String roomName = dataPack.getMessage(1);
+                    int roomId = parent.addRoom(roomName, userId);
+                    List<String> msgList = new ArrayList<>();
+                    msgList.add(String.valueOf(roomId));
+                    logger.info("Room created: " + roomId + " " + roomName);
+                    send(new DataPack(DataPack.ROOM_CREATE, new Date(), true, msgList));
+                    return;
+                }
+                case DataPack.ROOM_ENTER:{
+                    Integer userId = Integer.valueOf(dataPack.getMessage(0));
+                    Integer roomId = Integer.valueOf(dataPack.getMessage(1));
+                    Room room = parent.getRoom(roomId);
+                    if(room == null){
+                        send(new DataPack(DataPack.ROOM_ENTER, new Date(), false, null));
+                    }
+                    else{
+                        // if room has reached its limit
+                        if(room.getUsers().size() == 4){
+                            send(new DataPack(DataPack.ROOM_ENTER, new Date(), false, null));
+                        }
+                        else{
+                            // add user into room
+                            int position = room.addUser(userId);
+
+                            // send room user info back
+                            List<String> msgList = new ArrayList<>();
+                            msgList.add(String.valueOf(position));
+                            for(Integer roomUserId : room.getUsers()){
+                                // add user id
+                                msgList.add(String.valueOf(roomUserId));
+                                // add user name
+                                msgList.add(Database.getUser(roomUserId).get(1));
+                            }
+                            send(new DataPack(DataPack.ROOM_ENTER, new Date(), true, msgList));
+
+                            // send new user info to other users
+                            msgList.clear();
+                            msgList.add(String.valueOf(userId));
+                            List<String> userInfo = Database.getUser(userId);
+                            msgList.add(userInfo.get(1));
+                            msgList.add(userInfo.get(3));
+                            msgList.add(String.valueOf(position));
+                            for(Integer roomUserId : room.getUsers()){
+                                parent.getUserCommunicationJob(roomUserId)
+                                        .send(new DataPack(DataPack.ROOM_USER_ENTERED, new Date(), true, msgList));
+                            }
+                        }
+                    }
+                    return;
+                }
+                case DataPack.ROOM_LOOKUP:{
+                    List<String> msgList = new ArrayList<>();
+                    for(Room room : parent.getRooms()){
+                        // add room id
+                        msgList.add(String.valueOf(room.getId()));
+                        // add room name
+                        msgList.add(room.getName());
+                        // add number of players in the room
+                        msgList.add(String.valueOf(room.getUsers().size()));
+                    }
+                    send(new DataPack(DataPack.LOGIN, new Date(), true, msgList));
+                    return;
+                }
+                case DataPack.ROOM_EXIT:{
+                    Integer userId = Integer.valueOf(dataPack.getMessage(0));
+                    Integer roomId = Integer.valueOf(dataPack.getMessage(1));
+                    Room room = parent.getRoom(roomId);
+                    room.removeUser(userId);
+                    send(new DataPack(DataPack.ROOM_EXIT, new Date(), true, null));
+
+                    List<String> msgList = new ArrayList<>();
+                    msgList.add(String.valueOf(userId));
+                    // send user left message to other users
+                    for(Integer roomUserId : room.getUsers()){
+                        parent.getUserCommunicationJob(roomUserId)
+                                .send(new DataPack(DataPack.ROOM_USER_LEFT, new Date(), true, msgList));
+                    }
+                    return;
+                }
+                case DataPack.GAME_START:{
+                    Integer userId = Integer.valueOf(dataPack.getMessage(0));
+                    Integer roomId = Integer.valueOf(dataPack.getMessage(1));
+                    Room room = parent.getRoom(roomId);
+
+                    // send out game start signal and positions info to the users
+                    for(Integer roomUserId : room.getUsers()) {
+                        parent.getUserCommunicationJob(roomUserId)
+                                .send(new DataPack(DataPack.GAME_START, new Date(), true, null));
+                    }
+                    return;
+                }
+                // the following 2 commands' logic is basically the same(simply forward the datapack)
+                case DataPack.GAME_FINISHED:
+                case DataPack.GAME_PROCEED:{
+                    Integer roomId = Integer.valueOf(dataPack.getMessage(1));
+                    Room room = parent.getRoom(roomId);
+
+                    // update datapack's time
+                    dataPack.setDate(new Date());
+
+                    // simply forward the datapack to the users in the same room
+                    for(Integer roomUserId : room.getUsers()) {
+                        parent.getUserCommunicationJob(roomUserId)
+                                .send(dataPack);
+                    }
                     return;
                 }
                 default:
                     return;
             }
-        } catch(Exception e){
+        } catch(IndexOutOfBoundsException e){
+            logger.warn("DataPack missing necessary parameters.");
+        }
+        catch(Exception e){
             logger.catching(e);
         }
     }
