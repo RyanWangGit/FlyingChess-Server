482adf9e3ae29354e74f8a3426c1f07f5130fe11
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-04-27 23:27:11 +0800

    Add `Player` class which inherits from `User`, which controls dynamic data.

---
+++ src/main/java/GameObjects/Player.java
@@ -0,0 +1,43 @@
+package GameObjects;
+
+import Server.DataPackSocket;
+
+/**
+ * Created by Ryan on 16/4/27.
+ */
+public class Player extends User {
+    private boolean isHost = false;
+    private DataPackSocket socket = null;
+
+    public Player(User user, DataPackSocket socket, boolean isHost){
+        super(user);
+        this.socket = socket;
+        this.isHost = isHost;
+    }
+
+    public Player(User user, DataPackSocket socket){
+        super(user);
+        this.socket = socket;
+    }
+
+    public boolean isHost() { return this.isHost; }
+
+    public void setHost(boolean isHost) { this.isHost = isHost; }
+
+    public void setSocket(DataPackSocket socket) { this.socket = socket; }
+
+    public DataPackSocket getSocket() { return this.socket; }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null)
+            return false;
+
+        if(obj instanceof Player){
+            Player player = (Player) obj;
+            return this.id == player.id;
+        }
+
+        return false;
+    }
+}
