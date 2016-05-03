ce90814a109f9cf8c926edfa019042b23d6aaad7
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-05-03 13:40:27 +0800

    Add `DataPackUtil` class to put some utility functions for generating msgList.

--- src/main/java/Managers/PlayerManager.java
+++ src/main/java/Managers/PlayerManager.java
@@ -1,8 +1,9 @@
 package Managers;
 
 import DataPack.DataPack;
-import GameObjects.Player;
+import GameObjects.Player.Player;
 import GameObjects.User;
+import edu.emory.mathcs.backport.java.util.Collections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,48 +17,39 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class PlayerManager {
     private static Logger logger = LogManager.getLogger(PlayerManager.class.getName());
-    private Map<Integer, Player> players = null;
+    private Map<Integer, Player> allPlayers = null;
 
     public PlayerManager(){
-        this.players = new ConcurrentHashMap<>(1000, 0.75f);
+        this.allPlayers = new ConcurrentHashMap<>(100, 0.75f);
     }
+
     public Player getPlayer(int id) {
         // if it is a robot
-        if(id < 0 && id >= -4){
+        if(id < 0 && id >= -4)
             return new Player(new User(id, "Robot", null, 0), null);
-        }
         else
-            return this.players.get(id);
+            return this.allPlayers.get(id);
+
     }
 
-    public void addPlayer(Player player){
-        Player currentPlayer = this.players.get(player.getId());
-        if(currentPlayer != null){
-            if(currentPlayer.getSocket().equals(player.getSocket()))
-                return;
-            // remove current
-            removePlayer(currentPlayer);
-        }
-
-        this.players.put(player.getId(), player);
-        return;
-    }
+    public void addPlayer(Player player) { this.allPlayers.put(player.getId(), player); }
 
     public void removePlayer(Player player){
         try{
             player.getSocket().send(new DataPack(DataPack.TERMINATE));
             player.getSocket().close();
-            this.players.remove(player.getId());
+            this.allPlayers.remove(player.getId());
         } catch(IOException e){
             logger.catching(e);
         }
     }
 
     public void removePlayer(int playerId){
-        removePlayer(this.players.get(playerId));
+        removePlayer(this.allPlayers.get(playerId));
     }
 
-    public Collection<Player> getPlayers(){
-        return this.players.values();
+    public Collection<Player> getAllPlayers(){
+        return Collections.unmodifiableCollection(this.allPlayers.values());
     }
+
 }
