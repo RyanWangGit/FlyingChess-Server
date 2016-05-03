b3985cb43e882dfef6318c54c0cc3cfd4b754831
RyanWangGit <wangyuxin@hust.edu.cn>
2016-05-03 13:43:54 +0800

    Add `BroadcastThread` class.
    It can broadcast to all the players and can have a filter to filter out
    those you don’t want to send the data pack.

---
+++ src/main/java/GameObjects/Player/RoomSelectingFilter.java
@@ -0,0 +1,11 @@
+package GameObjects.Player;
+
+/**
+ * Created by Ryan on 16/5/2.
+ */
+public class RoomSelectingFilter implements PlayerFilter{
+
+    public boolean isBlocked(Player player){
+        return !(player.getStatus() == Player.ROOM_SELECTING);
+    }
+}
