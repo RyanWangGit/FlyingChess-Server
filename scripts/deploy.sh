20a23636ad837ff3efecde894fe4a815bfb4dce6
RyanWangGit <wangyuxin@hust.edu.cn>
2016-05-11 21:07:22 +0800

    Correct the way to access env variables.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/expect -f
 set timeout -1
-spawn scp ${JAR_WITH_DEPENDENCIES} ${USERNAME}@${SERVERIP}:${JARPATH}
+spawn scp $env(JAR_WITH_DEPENDENCIES) $env(USERNAME)@$env(SERVERIP):$env(JARPATH)
 expect {
     "(yes/no)?" {send "yes\n"; exp_continue}
-    "*assword:" {send "${PASSWORD}\n";} 
+    "*assword:" {send "$env(PASSWORD)\n";} 
 }
 expect "100%"
 spawn ssh root@ryanwang.cc
 expect {
     "(yes/no)?" {send "yes\n"; exp_continue}
-    "*assword:" {send "${PASSWORD}\n";} 
+    "*assword:" {send "$env(PASSWORD)\n";} 
 }
 expect "#"
 send "kill `jps \| grep \"flyingchess\" \| cut -d \" \" -f 1`\n"
 expect "#"
-send "nohup java -jar ${JARPATH} &\r\r"  
+send "nohup java -jar $env(JARPATH) &\r\r"  
 send "exit\r"
 expect eof
