30030f416cf5679f58ca9bb598abda0ad7f5dff9
Yuxin Wang <yuxinwang.dev@gmail.com>
2016-05-11 20:18:34 +0800

    Add auto build & deploy scripts.

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+set timeout -1
+spawn scp ${JAR_WITH_DEPENDENCIES ${USERNAME}@${SERVERIP}:${PATH}
+expect {
+    "(yes/no)?" {send "yes\n"; exp_continue}
+    "*assword:" {send "${PASSWORD}\n";} 
+}
+expect "100%"
+spawn ssh root@ryanwang.cc
+expect {
+    "(yes/no)?" {send "yes\n"; exp_continue}
+    "*assword:" {send "${PASSWORD}\n";} 
+}
+expect "#"
+send "kill `jps \| grep \"flyingchess\" \| cut -d \" \" -f 1`\n"
+expect "#"
+send "nohup java -jar ${PATH} &\r\r"  
+send "exit\r"
+expect eof
